6313167bda31067d988430af33a6f03466d16cd8
Diana <sgflower66@gmail.com>
2018-05-18 12:06:40 +0800

    12:00

--- homework_5/homework5/main.cpp
+++ homework_5/homework5/main.cpp
@@ -4,25 +4,38 @@
 
 void  main()
 {
-	char str[100];
-	char c;
-	int length,i;
-	//scanf("%s",str);
-	c=getchar();
-	//length=strlen(str);
-	if (c == ' ');
-	printf("kongge");
-	/*for (i = 0; i < length; i++)
+	char longest_word(int n, char str[100]);
+	char str[100] = { 0 }, a[100] = {0};
+	int i;
+	gets_s(str);
+
+
+
+	for (i = 0;a[i]!=' '; i++)
 	{
-		if isspace(str[i])
+		a[i] = longest_word(i, str);
+	
 
-	}*/
-
-
-
-
-
+	}
+	puts(a);
 
 	system("pause");
 
+}
+char longest_word(int n, char str[100])
+{
+	char a[100] = {0}, b[100] = { 0 };
+	int i, j = 0, k, length_word = 0, m;
+	int length;
+	length = strlen(str);
+	for (i = 0,j=0; i < length;i++,j++)
+	{
+		a[j] = str[i];
+		if (str[i] == ' ');
+		{
+			break;
+		}
+	}
+	length_word = strlen(a);
+	return(b[n]);
 }
